e4ed1de3310d0ec744cdaa1be4d054970ffec258
unknown <landerp@opendeusto.es>
2020-05-22 16:38:51 +0200

    Datanucleus bien

--- src/db/create-productsdb.sql
+++ src/db/create-productsdb.sql
@@ -2,14 +2,14 @@
 DROP SCHEMA productsDB;
 
 /* DELETE USER 'products_user' AT LOCAL SERVER*/
-DROP USER 'uixjskf8gv6vwlpc'@'%';
+DROP USER 'db4hFGoBHE'@'%';
 
 /* CREATE ''productsDB' DATABASE */
 CREATE SCHEMA productsDB;
 
 /* CREATE THE USER 'products_user' AT LOCAL SERVER WITH PASSWORD 'password' */
 
-CREATE USER 'uixjskf8gv6vwlpc'@'%' IDENTIFIED BY 'dr9d0imb1vYbYI4rCJlr';
+CREATE USER 'db4hFGoBHE'@'%' IDENTIFIED BY 'zCt1wUkWlA';
 
 /* GRANT FULL ACCESS TO THE DATABASE FOR THE USER 'products_user' AT LOCAL SERVER*/
-GRANT ALL ON productsDB.* TO 'uixjskf8gv6vwlpc'@'%';
+GRANT ALL ON productsDB.* TO 'db4hFGoBHE'@'%';
